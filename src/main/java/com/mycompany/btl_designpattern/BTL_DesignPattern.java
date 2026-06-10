99d66f847ab032279b5bf6ec79c335b22dc2775b
NgocLuong <ngocluongg0101@gmail.com>
2026-06-10 13:20:24 +0700

    first commit

---
+++ src/main/java/com/mycompany/btl_designpattern/BTL_DesignPattern.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.btl_designpattern;
+
+/**
+ *
+ * @author ngocluong
+ */
+public class BTL_DesignPattern {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
