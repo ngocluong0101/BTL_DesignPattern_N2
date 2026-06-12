35d25916d0688425d2a6551350a8ba366e04c847
DinhAn <dinhan01022005@gmail.com>
2026-06-12 23:45:14 +0700

    add VoucherManagementController

--- src/main/java/com/clothingstore/controller/VoucherManagementController.java
+++ src/main/java/com/clothingstore/controller/VoucherManagementController.java
@@ -1,4 +1,21 @@
 package com.clothingstore.controller;
 
+package com.clothingstore.controller;
+
+import com.clothingstore.view.promotion.VoucherManagementView;
+
 public class VoucherManagementController {
+    private final VoucherManagementView view;
+
+    public VoucherManagementController() {
+        view = new VoucherManagementView();
+    }
+
+    public void showView() {
+        view.setVisible(true);
+    }
+
+    public void hideView() {
+        view.setVisible(false);
+    }
 }
